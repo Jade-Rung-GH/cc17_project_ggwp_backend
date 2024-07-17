ec26ff2e17065827bea3b5f78fbddce74863e66c
Jade-Rung-GH <jaitnipithe@hotmail.com>
2024-07-17 11:27:49 +0700

    finalize

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -8,7 +8,7 @@ const authController = {};
 
 authController.register = async (req, res, next) => {
   try {
-    console.log(req.body);
+    // console.log(req.body);
     // req.input password
     const data = req.input;
     const existUser = await userService.findUserByUsername(data.username);
